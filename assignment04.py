985c442245a88641195976abe97d8d00855f14cd
Olha Pashnieva <olha.pashnieva@gmail.com>
2019-10-28 13:40:32 +0200

    Completed assignment04

--- assignment04.py
+++ assignment04.py
@@ -6,7 +6,8 @@ def task1(text):
     >>> task1('89,9,-789, 0, 1')
     [89, 9, -789, 0, 1]
     """
-    # todo: write your code here
+    l = text.split(',')
+    return list(map(int, l))
 
 
 def task2(text):
@@ -16,7 +17,8 @@ def task2(text):
     >>> task2('pen pineapple apple pen')
     'apple pen pen pineapple'
     """
-    # todo: write your code here
+    l = sorted(text.split())
+    return ' '.join(l)
 
 
 def task3(text):
@@ -29,17 +31,26 @@ def task3(text):
     >>> task3('Найди себе дело по душе и тебе не придётся трудиться ни одного дня в жизни. (Конфуций)')
     {'digits': 0, 'letters': 26}
     """
-    # todo: write your code here
+    unique_val = ''.join(set(text))
+    digits = 0
+    letters = 0
+    for i in unique_val:
+        if i.isnumeric() is True:
+            digits += 1
+        elif i.isalpha() is True:
+            letters +=1
+    return {'digits': digits, 'letters': letters}
 
 
-def task4(digit):
+def task4(digit, n=4):
     """
     For any digit X from 0 to 9, calculate expression 'X + XX + XXX + XXXX'.
 
     >>> [task4(d) for d in '0123456789']
     [0, 1234, 2468, 3702, 4936, 6170, 7404, 8638, 9872, 11106]
     """
-    # todo: write your code here
+    if 0<=int(digit)<=9:
+        return sum([int(digit*i) for i in range(1, n+1)])
 
 
 def task5(text, letter1, letter2):
@@ -59,7 +70,7 @@ def task5(text, letter1, letter2):
     >>> task5('happy birthday', 'z', 'a')
     False
     """
-    # todo: write your code here
+    return -1 < text.rfind(letter1) < text.find(letter2)
 
 
 def task6(text, censored):
@@ -73,7 +84,9 @@ def task6(text, censored):
     >>> task6('*PP*RC*S*', 'UEAE')
     'UPPERCASE'
     """
-    # todo: write your code here
+    return ''.join(
+            ''.join(t) for t in zip(text.split('*'), [*censored, ''])
+    )
 
 
 def task7(text, words):
@@ -90,7 +103,7 @@ def task7(text, words):
     >>> task7('Jeff Goldblum', ['jog', 'meld', 'bluffs'])
     False
     """
-    # todo: write your code here
+    return all([set(w.lower()) == set(text.lower()).intersection(w.lower()) for w in words])
 
 
 if __name__ == '__main__':
